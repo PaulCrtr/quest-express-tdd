f6d64beb774924843d415d43e23c688655fb10e3
PaulCrtr <paul.th.cartier@gmail.com>
2020-06-08 11:53:12 +0200

    first exercices done

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.status(200).json({ message: "Hello World!" });
+});
+
+app.post("/bookmarks", (req, res) => {
+  if (!req.body.url || !req.body.title) {
+    res.status(422).json({ error: "required field(s) missing" });
+  } else {
+    connection.query("INSERT INTO bookmark SET ?", req.body, (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Failed to save a bookmark");
+      } else {
+        connection.query(
+          `SELECT * FROM bookmark WHERE id=${results.insertId}`,
+          (err, results2) => {
+            res.status(201).json(results2[0]);
+          }
+        );
+      }
+    });
+  }
 });
 
 module.exports = app;
